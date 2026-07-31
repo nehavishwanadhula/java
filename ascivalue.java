5be603461310760ecc14cf6f96cf2c32b53c3a66
nehavishwanadhula <nehavishwanadhula@gmail.com>
2026-07-31 14:49:52 +0530

    Add files via upload

---
+++ ascivalue.java
@@ -0,0 +1,9 @@
+public class ASCIIValue {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        int ascii = ch;
+
+        System.out.println("ASCII Value of " + ch + " = " + ascii);
+    }
+}
